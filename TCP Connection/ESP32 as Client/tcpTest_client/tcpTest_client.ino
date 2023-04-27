53091819aed0973c72ab30d889ce35845b41a7de
rex-g-2022 <113227615+rex-g-2022@users.noreply.github.com>
2023-04-27 02:48:35 -0700

    Add files via upload

---
+++ TCP Connection/ESP32 as Client/tcpTest_client/tcpTest_client.ino
@@ -0,0 +1,74 @@
+
+#include <WiFi.h>
+
+const char *ssid_Router     =  "SalmonMeatball";  //Enter the router name  "Berkeley-IoT"
+const char *password_Router =  "20220804";        //Enter the router password  "5D&sl3k#"
+#define     REMOTE_IP          "10.0.0.38"        //input the remote server which is you want to connect 
+#define     REMOTE_PORT         8888              //input the remote port which is the remote provide
+WiFiClient client;
+
+const int poten = 34;
+int poten_value;
+String poten_value_String;
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  WiFi.begin(ssid_Router, password_Router);
+  Serial.print("\nWaiting for WiFi... ");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
+
+  Serial.print("Connecting to ");
+  Serial.println(REMOTE_IP);
+
+  while (!client.connect(REMOTE_IP, REMOTE_PORT)) {
+    Serial.println("Connection failed.");
+    Serial.println("Waiting a moment before retrying...");
+  }
+  Serial.println("Connected");
+
+}
+
+void loop() {
+
+  /*
+    poten_value = analogRead(poten);
+    poten_value_String = String(poten_value);
+    client.print(poten_value_String.length());    //transmit the length of the data
+    client.print(poten_value);                    //transmit the data itself
+
+    Serial.print(poten_value_String.length());
+    Serial.print('\t');
+    Serial.println(poten_value);
+  */
+
+  if (client.connected() > 0) {
+
+
+    poten_value = analogRead(poten);
+    poten_value_String = String(poten_value);
+    client.print(poten_value_String.length());    //transmit the length of the data
+    client.print(poten_value);                    //transmit the data itself
+
+    Serial.print(poten_value_String.length());
+    Serial.print('\t');
+    Serial.println(poten_value);
+  }
+
+  if (client.connected () == 0) {
+    client.stop();
+    WiFi.disconnect();
+  }
+
+  delay(100);
+}
